f437e4d18cc430539b1d5c222f6a2aa8046672c7
mcontreras <migueljosecontreras@gmail.com>
2022-07-29 15:23:32 -0400

    force to https in production

--- api/resources/views/reports/report-bro/build.blade.php
+++ api/resources/views/reports/report-bro/build.blade.php
@@ -6,9 +6,9 @@
 
     <title>Report Bro</title>
 
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/plugins/report-bro/dist/reportbro.css', [], str_contains(\Config::get('app.url'), 'https://')) }}"/>
-    <script src="{{ url('assets/plugins/report-bro/dist/ext/jquery.js', [], str_contains(\Config::get('app.url'), 'https://')) }}"></script>
-    <script src="{{ url('assets/plugins/report-bro/dist/reportbro.min.js', [], str_contains(\Config::get('app.url'), 'https://')) }}"></script>
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/plugins/report-bro/dist/reportbro.css') }}"/>
+    <script src="{{ url('assets/plugins/report-bro/dist/ext/jquery.js') }}"></script>
+    <script src="{{ url('assets/plugins/report-bro/dist/reportbro.min.js') }}"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -30,7 +30,7 @@
     var oldReport = JSON.parse('{!! json_encode($version->formatted_report) !!}');
 </script>
 
-<script type="text/javascript" src="{{ url('assets/js/report-bro/scripts.js', [], str_contains(\Config::get('app.url'), 'https://')) }}"></script>
+<script type="text/javascript" src="{{ url('assets/js/report-bro/scripts.js') }}"></script>
 
 </body>
 </html>
