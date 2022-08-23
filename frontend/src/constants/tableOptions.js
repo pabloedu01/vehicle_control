9c091ae613bc1eb132b0dc29b0d15f0fa1c6eecf
serpams <eng.mserpa@gmail.com>
2022-08-23 10:21:13 -0300

    add sizePerPageList in TableOptions

--- frontend/src/constants/tableOptions.js
+++ frontend/src/constants/tableOptions.js
@@ -4,7 +4,24 @@ const TABLE_OPTIONS = {
     tableBodyWidth: '100%',
     responsive: 'vertical',
     tableBodyHeight: 'calc(45vh)',
-    searchText: ''
+    searchText: '',
+    sizePerPageList:[
+        {
+            text: '5',
+            value: 5,
+        },
+        {
+            text: '10',
+            value: 10,
+        },
+        {
+            text: '25',
+            value: 25,
+        },
+        {
+            text: 'All',
+            value: 50000,
+        }],
 };
 
 export default TABLE_OPTIONS;
