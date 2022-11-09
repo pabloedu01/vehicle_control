834872edadd2c6d2ecf7c0a6f93347f13989c13a
mcontreras <migueljosecontreras@gmail.com>
2022-11-09 08:46:11 -0400

    migracion

--- api/database/migrations/2022_10_31_153529_update_value2_to_checklist_item_vehicle_service_table.php
+++ api/database/migrations/2022_10_31_153529_update_value2_to_checklist_item_vehicle_service_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateValueToChecklistItemVehicleServiceTable extends Migration
+class UpdateValue2ToChecklistItemVehicleServiceTable extends Migration
 {
     /**
      * Run the migrations.
