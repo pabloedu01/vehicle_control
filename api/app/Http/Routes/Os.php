bf673051ca1e2d34d1104b6c42837958a643a542
serpams <eng.mserpa@gmail.com>
2023-02-01 08:21:28 -0300

    add collections

---
+++ api/app/Http/Routes/Os.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+                 'prefix'     => 'os',
+                 'middleware' => [ 'jwt.verify', 'user' ],
+             ], function(){
+
+    Route::group([
+                     'middleware' => [ 'company' ],
+                 ], function(){
+
+
+        Route::get('/', [
+            'uses' => 'OsTypeController@index',
+        ]);
+
+        Route::post('/', [
+            'uses' => 'OsTypeController@store',
+        ]);
+    });
+
+    Route::group([
+                     'middleware' => [ 'recommendation' ],
+                 ], function(){
+
+        Route::get('{id}', [
+            'uses' => 'OsTypeController@show',
+        ]);
+
+        Route::put('{id}', [
+            'uses' => 'OsTypeController@update',
+        ]);
+
+        Route::delete('{id}', [
+            'uses' => 'OsTypeController@destroy',
+        ]);
+    });
+
+
+});
