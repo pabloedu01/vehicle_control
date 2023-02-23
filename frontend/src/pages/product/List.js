53f9a124feb81ff981dec9f5f0775ce20f19d206
Kleber Freire <ch4r4d4@gmail.com>
2023-02-23 11:09:36 -0300

    subindo novamente

--- frontend/src/pages/product/List.js
+++ frontend/src/pages/product/List.js
@@ -12,6 +12,8 @@ import swal from 'sweetalert';
 
 const api = new APICore();
 
+console.log(`aqui`)
+
 const List = (props: {company?: any}): React$Element<React$FragmentType> => {
     const history = useNavigate();
     const [list, setList] = useState([]);
