c987940dc312fd0997931ab3f237dafbc16fec35
Kleber Freire <ch4r4d4@gmail.com>
2023-03-01 14:16:26 -0300

    modal de sugestao de orcamentos ok em agendamentos de serviços

--- frontend/src/pages/service-schedule/ModalSelectTypeMaintenanceReview/index.js
+++ frontend/src/pages/service-schedule/ModalSelectTypeMaintenanceReview/index.js
@@ -1,5 +1,12 @@
+import {useEffect, useState} from 'react';
 import { Modal, Button } from 'react-bootstrap'
 import  { useNavigate} from 'react-router-dom'
+
+import { APICore } from "../../../helpers/api/apiCore";
+
+
+const api = new APICore();
+
 
 export function ModalSelectTypeMaintenanceReview({
     openModalSelectTypeMaintenanceReview,
@@ -8,50 +15,39 @@ export function ModalSelectTypeMaintenanceReview({
     id
 }) {
 
+
+    const [maintenanceReviewButtons, setMaintenanceReviewButtons] = useState([]);
+
+    const history = useNavigate();
+    
     function onHideModal() {
         setOpenModalSelectTypeMaintenanceReview(false)
     }
 
-    const history = useNavigate();
+    useEffect(() => {
+        if (openModalSelectTypeMaintenanceReview) {
+            api.get(`/maintenance-review?company_id=2&model_id=1`).then(response => { setMaintenanceReviewButtons(response.data.data) })
+        }
+    },[openModalSelectTypeMaintenanceReview])
+
 
     return (
         <Modal show={openModalSelectTypeMaintenanceReview} onHide={onHideModal}>
                 <Modal.Header onHide={onHideModal} closeButton>
                     <h4 className="modal-title">Tipo de orçamento</h4>
                 </Modal.Header>
-                <Modal.Body>
+            <Modal.Body>
+                {maintenanceReviewButtons.map((maintenanceReview, index) => (
                     <Button 
                         variant="Primary"                             
                         className="btn btn-primary w-100 mb-2"
-                        onClick={() => { history(`/panel/company/${company_id}/workshop/quotation/create/${id}`) }}
+                        onClick={() => { history(`/panel/company/${maintenanceReview.company_id}/workshop/quotation/create/${id}`) }}
                         disabled
+                        key={maintenanceReview.name + maintenanceReview.id + index}
                     >
-                        1ª Revisão
+                        {maintenanceReview.name}
                     </Button>
-                    <Button 
-                        variant="Primary"                             
-                        className="btn btn-primary w-100 mb-2"
-                        onClick={() => { history(`/panel/company/2/workshop/quotation/create/${id}`) }}
-                        disabled
-                    >
-                        2ª Revisão
-                    </Button>
-                    <Button 
-                        variant="Primary"                             
-                        className="btn btn-primary w-100 mb-2"
-                        onClick={() => { history(`/panel/company/2/workshop/quotation/create/${id}`) }}
-                        disabled
-                    >
-                        3ª Revisão
-                    </Button>
-                    <Button 
-                        variant="Primary"                             
-                        className="btn btn-primary w-100 mb-2"
-                        onClick={() => { history(`/panel/company/2/workshop/quotation/create/${id}`) }}
-                        disabled
-                    >
-                        4ª Revisão
-                    </Button>
+                ))}
                     <Button 
                         variant="Primary"                             
                         className="btn btn-primary w-100 mb-2"
