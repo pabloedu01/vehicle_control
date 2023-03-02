06819bde1b8d4b1261a371c7f2d1c73b82ea2531
devgmaisp <mateus@gmaisp.com.br>
2023-03-02 16:15:50 -0300

    fix recomendation updates

--- api/app/Http/Controllers/RecommendationController.php
+++ api/app/Http/Controllers/RecommendationController.php
@@ -72,7 +72,10 @@ class RecommendationController extends Controller
         $recomendation->os_type_id = $request['os_type_id'];
         $recomendation->save();
 
+            // delete
+
         if($request['services'] != null or $request['services'] != []) {
+
             foreach ($request['services'] as $service) {
                 // Verifica se ja existe serviços iguais nesta recomendação
                 $checkexist = RecommendationServices::where('recommendation_id', $recomendation->id)
@@ -99,7 +102,7 @@ class RecommendationController extends Controller
                 ->first();
                 if($checkexists){
                     // trow new exception
-                    throw new \Exception("Product not found in this Company");
+                    throw new \Exception("Product has been added in this recomendations");
                 }
 
 
@@ -150,7 +153,8 @@ class RecommendationController extends Controller
 
     public function update(Request $request, $id)
     {
-        $recommendation = Recommendation::where('id', $request['recommendation_id'])->first();
+        DB::beginTransaction();
+        $recommendation = Recommendation::where('id', $id)->first();
         $recommendation->name = $request['description'];
         $recommendation->vehicle_id = $request['vehicle_id'];
         $recommendation->model_id = $request['model_id'] ?? null;
@@ -159,74 +163,63 @@ class RecommendationController extends Controller
         $recommendation->service_type_id = $request['service_type_id'];
         $recommendation->save();
 
+        try {
+
         if($request['services'] != null or $request['services'] != []) {
+
+        /// delete services
+        RecommendationServices::where('recommendation_id', $id)->delete();
+
             foreach ($request['services'] as $service) {
-                $recomendationService = RecommendationServices::where('recommendation_id', $request['recommendation_id'])->where('service_id', $service['id'])->first();
-                if($recomendationService == null) {
+                $checkexist = RecommendationServices::where('recommendation_id', $id)
+                ->where('service_id', $service['service_id'])
+                ->first();
+                if($checkexist){
+                    // trow new exception
+                    throw new \Exception("Service has been added in this recomendations");
+                }else {
                     $recomendationService = new RecommendationServices();
-                    $recomendationService->recommendation_id = $recommendation->id;
+                    $recomendationService->recommendation_id = $id;
                     $recomendationService->service_id = $service['id'];
                     $recomendationService->quantity = $service['quantity'];
                     $recomendationService->save();
-                } else {
-                    $recomendationService->quantity = $service['quantity'];
-                    $recomendationService->save();
                 }
-            }
-                // verifica se o services recebido esta igual ao service persistido ao final se não deleta o service persistido
-                $services = RecommendationServices::where('recommendation_id', $request['recommendation_id'])->get();
-                foreach ($services as $service) {
-                    $serviceExist = false;
-                    foreach ($request['services'] as $serviceRequest) {
-                        if($serviceRequest['id'] == $service->service_id) {
-                            $serviceExist = true;
-                        }
-                    }
-                    if($serviceExist == false) {
-                        $service->delete();
-                    }
-                }
+
+
         }
         if($request['products'] != null or $request['products'] != []) {
+            // delete products
+            RecommendationProducts::where('recommendation_id', $id)->delete();
+
             foreach ($request['products'] as $product) {
-                $recomendationProduct = RecommendationProducts::where('recommendation_id', $request['recommendation_id'])->where('product_id', $product['id'])->first();
-                if($recomendationProduct == null) {
+                    $checkexists = RecommendationProducts::where('recommendation_id', $id)
+                    ->where('product_id', $product['product_id'])
+                    ->first();
+                if($checkexists){
+                    // trow new exception
+                    throw new \Exception("Product has been added in this recomendations");
+                }
                     $recomendationProduct = new RecommendationProducts();
-                    $recomendationProduct->recommendation_id = $recommendation->id;
+                    $recomendationProduct->recommendation_id = $id;
                     $recomendationProduct->product_id = $product['id'];
                     $recomendationProduct->quantity = $product['quantity'];
                     $recomendationProduct->save();
-                } else {
-                    $recomendationProduct->quantity = $product['quantity'];
-                    $recomendationProduct->save();
-                }
+
             }
-                // verifica se o products recebido esta igual ao product persistido ao final se não deleta o product persistido
-                $products = RecommendationProducts::where('recommendation_id', $request['recommendation_id'])->get();
-                foreach ($products as $product) {
-                    $productExist = false;
-                    foreach ($request['products'] as $productRequest) {
-                        if($productRequest['id'] == $product->product_id) {
-                            $productExist = true;
-                        }
-                    }
-                    if($productExist == false) {
-                        $product->delete();
-                    }
-                }
+
         }
         if($request['claim_services'] != null or $request['claim_services'] != []) {
             foreach ($request['claim_services'] as $claimService) {
-                $recomendationClaimService = RecommendationClaimService::where('recommendation_id', $request['recommendation_id'])->where('claims_service_id', $claimService['id'])->first();
+                $recomendationClaimService = RecommendationClaimService::where('recommendation_id', $id)->where('claims_service_id', $claimService['id'])->first();
                 if($recomendationClaimService == null) {
                     $recomendationClaimService = new RecommendationClaimService();
-                    $recomendationClaimService->recommendation_id = $recommendation->id;
+                    $recomendationClaimService->recommendation_id = $id;
                     $recomendationClaimService->claims_service_id = $claimService['id'];
                     $recomendationClaimService->save();
                 }
             }
                 // verifica se o claim_services recebido esta igual ao claim_service persistido ao final se não deleta o claim_service persistido
-                $claimServices = RecommendationClaimService::where('recommendation_id', $request['recommendation_id'])->get();
+                $claimServices = RecommendationClaimService::where('recommendation_id', $id)->get();
                 foreach ($claimServices as $claimService) {
                     $claimServiceExist = false;
                     foreach ($request['claim_services'] as $claimServiceRequest) {
@@ -238,11 +231,20 @@ class RecommendationController extends Controller
                         $claimService->delete();
                     }
                 }
-        }
+         }
+        }} catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json([
+                'msg'    => trans('general.msg.invalidData'),
+                'errors' => $th->getMessage(),
+            ],
+            Response::HTTP_BAD_REQUEST
+            );
+         }
 
-
+        DB::commit();
         $show = Recommendation::with(['vehicle', 'vehicle.model', 'vehicle.model.brand', 'maintenanceReview', 'claimService',  'products','products.product','services','services.service','model'])
-        ->where('id', '=', $request['recommendation_id'])
+        ->where('id', '=', $id)
         ->first();
 
 
